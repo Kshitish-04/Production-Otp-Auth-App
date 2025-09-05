35223082fc39edd82bba9e0fa92a0cd8cf3d7a12
Kshitish 04 <pradhankshitish1@gmail.com>
2025-09-06 01:06:10 +0530

    API / update

--- Frontend/src/Api.js
+++ Frontend/src/Api.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'https://production-otp-auth-app-m3x4.vercel.app//api/auth'})
+// const API = axios.create({baseURL: 'https://production-otp-auth-app-m3x4.vercel.app//api/auth'})
+const API = axios.create({baseURL: 'https://production-otp-auth-app-m3x4.vercel.app/api/auth'})
+
+
+
+
+
+
 
 export default API; 
