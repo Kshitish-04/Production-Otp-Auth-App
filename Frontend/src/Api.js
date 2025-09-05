ab4f24fbe3053ac7d29ea02a61eef18c262d1dcc
Kshitish 04 <pradhankshitish1@gmail.com>
2025-09-06 00:56:16 +0530

    API link base url updated

--- Frontend/src/Api.js
+++ Frontend/src/Api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:5000/api/auth'})
+const API = axios.create({baseURL: 'https://production-otp-auth-app-m3x4.vercel.app//api/auth'})
 
 export default API; 
